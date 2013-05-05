70fdce8c537eb95363cd2fcca87ae86eda502a1f
Jesse Smith <jesses16@gmail.com>
2013-05-05 12:24:36 -0700

    bubble up click event and use most recent view; refactor

--- openure.js
+++ openure.js
@@ -1,38 +1,56 @@
-myViews = [];
+Openure = {
+    allViews: [],
+    currentView: "",
+    listener: "",
 
-findViewsInRegion = function(region) {
-	console.log("region!");
-	findViewsInView(region.currentView);
-}
+    findViewsInRegion: function(region) {
+        this.findViewsInView(region.currentView);
+    },
 
-findViewsInView = function(view) {
-	console.log("view!");
-	myViews.push(view);
-	for(var prop in view) {
-		if (view[prop] instanceof Backbone.ChildViewContainer) {
-			view[prop].each(function(subview) {
-				findViewsInView(subview);
-			});
-		} else if (view[prop] instanceof Marionette.Region) {
-			findViewsInRegion(view[prop]);
-		}
-	}
-}
+    findViewsInView: function(view) {
+        for(var prop in view) {
+            if (view[prop] instanceof Backbone.ChildViewContainer) {
+                view[prop].each(function(subview) {
+                    this.findViewsInView(subview);
+                }, this);
+            } else if (view[prop] instanceof Marionette.Region) {
+                this.findViewsInRegion(view[prop]);
+            }
+        }
+        this.allViews.push(view);
+    },
 
-for(var prop in Fantasizr) {
-debugger
-	if (Fantasizr[prop] instanceof Marionette.Region) {
-		findViewsInRegion(Fantasizr[prop]);
-	} else if (Fantasizr[prop] instanceof Marionette.View) {
-		findViewsInView(Fantasizr[prop]);
-	}
-}
+    applySelectedView: function() {
+        console.log("rock the view - " + Openure.currentView.cid);
+        model = this.currentView.model;
+        view = this.currentView;
+        collection = this.currentView.collection;
+        clearInterval(this.listener);
+    },
 
-_.each(myViews, function(view) {
-	view.$el[0].addEventListener('click', function(e) {
-		if(e.metaKey && e.shiftKey) {
-			e.stopPropagation();
-		    console.log(view);
-		}
-	}, true);
-});
+    go: function() {
+        for(var prop in Fantasizr) {
+            if (Fantasizr[prop] instanceof Marionette.Region) {
+                this.findViewsInRegion(Fantasizr[prop]);
+            } else if (Fantasizr[prop] instanceof Marionette.View) {
+                this.findViewsInView(Fantasizr[prop]);
+            }
+        }
+
+        _.each(this.allViews, function(view) {
+            view.$el[0].addEventListener('click', _.bind(function(e) {
+                var ownIt = _.bind(this.applySelectedView, this);
+
+                clearInterval(this.listener);
+                this.listener = setInterval(ownIt, 100);
+
+                if(e.metaKey && e.shiftKey) {
+                    e.preventDefault();
+                    this.currentView = view;
+                }
+            }, this), true);
+        }, this);
+    }
+};
+
+Openure.go();
