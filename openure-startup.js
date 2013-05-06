a4d3d1df8fef2301a072814ed948be948f3030d8
Jesse Smith <jesses16@gmail.com>
2013-05-05 22:57:41 -0700

    initial implementation of jqconsole from repl.it

--- openure-startup.js
+++ openure-startup.js
@@ -2,3 +2,14 @@ var script = document.createElement('script');
 script.src = "https://raw.github.com/jesses16/openure/master/openure.js";
 script.type="text/javascript";
 document.head.appendChild(script);
+
+var jqscript = document.createElement('script');
+jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
+jqscript.type="text/javascript";
+document.head.appendChild(jqscript);
+
+
+var jqcscript = document.createElement('script');
+jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
+jqcscript.type="text/javascript";
+document.head.appendChild(jqcscript);
--- openure.js
+++ openure.js
@@ -39,6 +39,68 @@ Openure = {
 
     applySelectedView: function() {
         console.log("rock the view - " + Openure.currentView.cid);
+
+        var jqconsole = document.createElement('div');
+
+        jqconsole.style.cssText = 'position:absolute;width:400px;height:500px;background-color:black';
+        jqconsole.id = "console";
+
+        this.currentView.el.appendChild(jqconsole);
+
+// Creating the console.
+        var header = 'Welcome to JQConsole!\n' +
+            'Use jqconsole.Write() to write and ' +
+            'jqconsole.Input() to read.\n';
+        this.jqconsole = $('#console').jqconsole(header, 'JS> ');
+
+// Abort prompt on Ctrl+Z.
+        this.jqconsole.RegisterShortcut('Z', function() {
+            this.jqconsole.AbortPrompt();
+            handler();
+        });
+
+// Move to line start Ctrl+A.
+        this.jqconsole.RegisterShortcut('A', function() {
+            this.jqconsole.MoveToStart();
+            handler();
+        });
+
+// Move to line end Ctrl+E.
+        this.jqconsole.RegisterShortcut('E', function() {
+            this.jqconsole.MoveToEnd();
+            handler();
+        });
+
+        this.jqconsole.RegisterMatching('{', '}', 'brace');
+        this.jqconsole.RegisterMatching('(', ')', 'paran');
+        this.jqconsole.RegisterMatching('[', ']', 'bracket');
+// Handle a command.
+        var handler = _.bind(function(command) {
+            if (command) {
+                try {
+                    this.jqconsole.Write('==> ' + window.eval(command) + '\n');
+                } catch (e) {
+                    this.jqconsole.Write('ERROR: ' + e.message + '\n');
+                }
+            }
+            this.jqconsole.Prompt(true, handler, function(command) {
+                // Continue line if can't compile the command.
+                try {
+                    Function(command);
+                } catch (e) {
+                    if (/[\[\{\(]$/.test(command)) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                }
+                return false;
+            });
+        }, this);
+
+// Initiate the first prompt.
+        handler();
+
         model = this.currentView.model;
         view = this.currentView;
         collection = this.currentView.collection;
