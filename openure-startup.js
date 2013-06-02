b556093201f04563580173a6f8c07c5d64f70397
sosaucily <sosaucily@gmail.com>
2013-06-01 20:23:26 -0700

    Add options page; point to sosaucily repo; loop through objects differently.

--- openure-startup.js
+++ openure-startup.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = "https://raw.github.com/jesses16/openure/master/openure.js";
+script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
 script.type="text/javascript";
 document.head.appendChild(script);
 
@@ -8,8 +8,18 @@ jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
 jqscript.type="text/javascript";
 document.head.appendChild(jqscript);
 
-
 var jqcscript = document.createElement('script');
 jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
 jqcscript.type="text/javascript";
 document.head.appendChild(jqcscript);
+
+chrome.extension.sendRequest({method: "getOpenureKeys"}, function(response) {
+    if(!response){
+        return;
+    }
+    var openure_keys = document.createElement('openure_keys');
+    openure_keys.type="text/html";
+    var newContent = document.createTextNode(response.keys);
+    openure_keys.appendChild(newContent);
+    document.head.appendChild(openure_keys);
+});
