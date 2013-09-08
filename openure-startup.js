cae76897081c9597a0a49b0402d51cfdfbd13a62
Jesse Smith <sosaucily@gmail.com>
2013-09-07 18:45:52 -0700

    Properly format files

--- openure-startup.js
+++ openure-startup.js
@@ -1,24 +1,24 @@
 var script = document.createElement('script');
 script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
-script.type="text/javascript";
+script.type = "text/javascript";
 document.head.appendChild(script);
 
 var jqscript = document.createElement('script');
 jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
-jqscript.type="text/javascript";
+jqscript.type = "text/javascript";
 document.head.appendChild(jqscript);
 
 var jqcscript = document.createElement('script');
 jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
-jqcscript.type="text/javascript";
+jqcscript.type = "text/javascript";
 document.head.appendChild(jqcscript);
 
-chrome.extension.sendRequest({method: "getOpenureKeys"}, function(response) {
-    if(!response){
+chrome.extension.sendRequest({method: "getOpenureKeys"}, function (response) {
+    if (!response) {
         return;
     }
     var openure_keys = document.createElement('openure_keys');
-    openure_keys.type="text/html";
+    openure_keys.type = "text/html";
     var newContent = document.createTextNode(response.keys);
     openure_keys.appendChild(newContent);
     document.head.appendChild(openure_keys);
--- options.js
+++ options.js
@@ -1,32 +1,32 @@
 // Saves options to localStorage.
 function save_options() {
-  var select = document.getElementById("openure-key");
-  var keyName = select.value;
-  localStorage["openure_keys"] = keyName;
+    var select = document.getElementById("openure-key");
+    var keyName = select.value;
+    localStorage["openure_keys"] = keyName;
 
-  // Update status to let user know options were saved.
-  var status = document.getElementById("status");
-  status.innerHTML = "Options Saved.";
-  setTimeout(function() {
-    status.innerHTML = "";
-  }, 750);
+    // Update status to let user know options were saved.
+    var status = document.getElementById("status");
+    status.innerHTML = "Options Saved.";
+    setTimeout(function () {
+        status.innerHTML = "";
+    }, 750);
 }
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-  var keyName = localStorage["openure_keys"];
-  if (!keyName) {
-    return;
-  }
-  var select = document.getElementById("openure-key");
-  select.value = keyName;
+    var keyName = localStorage["openure_keys"];
+    if (!keyName) {
+        return;
+    }
+    var select = document.getElementById("openure-key");
+    select.value = keyName;
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
     if (request.method == "getOpenureKeys")
-      sendResponse({keys: localStorage['openure_keys']});
+        sendResponse({keys: localStorage['openure_keys']});
     else
-      sendResponse({}); // snub them.
+        sendResponse({}); // snub them.
 });
