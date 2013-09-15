f29b6a59eeccfc8772cec028063b63071f337986
brainfish <brian@mopub.com>
2013-09-15 14:50:37 -0700

    Fix #13: Inline dependency scripts and avoid jQuery conflicts

--- openure-startup.js
+++ openure-startup.js
@@ -9,21 +9,6 @@ script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
 script.type = "text/javascript";
 document.head.appendChild(script);
 
-var jqscript = document.createElement('script');
-jqscript.src = "http://code.jquery.com/jquery-1.8.3.min.js";
-jqscript.type = "text/javascript";
-document.head.appendChild(jqscript);
-
-var jqcscript = document.createElement('script');
-jqcscript.src = "http://code.jquery.com/ui/1.10.3/jquery-ui.min.js";
-jqcscript.type = "text/javascript";
-document.head.appendChild(jqcscript);
-
-var jqcscript = document.createElement('script');
-jqcscript.src = "https://raw.github.com/replit/jq-console/master/jqconsole.min.js";
-jqcscript.type = "text/javascript";
-document.head.appendChild(jqcscript);
-
 chrome.extension.sendRequest({method: "getOpenureKeys"}, function (response) {
     if (!response) {
         return;
