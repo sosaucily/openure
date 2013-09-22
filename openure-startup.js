65f57b5b5c9977b830044cf8a015c7d274276f60
Jesse Smith <sosaucily@gmail.com>
2013-09-21 18:16:04 -0700

    [Close #19] Use chrome popup page to configure openure.

--- openure-startup.js
+++ openure-startup.js
@@ -1,15 +1,21 @@
-var script = document.createElement('script');
-script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
-script.type = "text/javascript";
-document.head.appendChild(script);
+var openure_keys = document.createElement('openure_keys');
+openure_keys.type = "text/html";
 
-chrome.extension.sendRequest({method: "getOpenureKeys"}, function (response) {
-    if (!response) {
-        return;
+chrome.storage.local.get(["openure_key","openure_development"], function (openure_data){
+
+    if (openure_data['openure_key']) {
+
+        var newContent = document.createTextNode(openure_data['openure_key']);
+        openure_keys.appendChild(newContent);
+        document.head.appendChild(openure_keys);
+
+        var script = document.createElement('script');
+        script.src = "https://raw.github.com/sosaucily/openure/master/openure.js";
+        if (openure_data['openure_development']) {
+            console.log('Running Openure in development mode from the grunt server');
+            script.src = "https://localhost:9000/openure.js";
+        }
+        script.type = "text/javascript";
+        document.head.appendChild(script);
     }
-    var openure_keys = document.createElement('openure_keys');
-    openure_keys.type = "text/html";
-    var newContent = document.createTextNode(response.keys);
-    openure_keys.appendChild(newContent);
-    document.head.appendChild(openure_keys);
 });
--- options.js
+++ options.js
@@ -2,7 +2,11 @@
 function save_options() {
     var select = document.getElementById("openure-key");
     var keyName = select.value;
-    localStorage["openure_keys"] = keyName;
+    chrome.storage.local.set({"openure_key":keyName});
+
+    var select = document.getElementById("openure-development");
+    var keyName = select.value;
+    chrome.storage.local.set({"openure_development":keyName});
 
     // Update status to let user know options were saved.
     var status = document.getElementById("status");
@@ -14,19 +18,20 @@ function save_options() {
 
 // Restores select box state to saved value from localStorage.
 function restore_options() {
-    var keyName = localStorage["openure_keys"];
-    if (!keyName) {
-        return;
-    }
-    var select = document.getElementById("openure-key");
-    select.value = keyName;
+    chrome.storage.local.get(["openure_key","openure_development"], function (openure_data){
+        var keyName = openure_data["openure_key"];
+        if (keyName) {
+            var select = document.getElementById("openure-key");
+            select.value = keyName;
+        }
+
+        var keyName = openure_data["openure_development"];
+        if (!keyName) {
+            var select = document.getElementById("openure-development");
+            select.value = keyName;
+        }
+    });
 }
+
 document.addEventListener('DOMContentLoaded', restore_options);
 document.querySelector('#save').addEventListener('click', save_options);
-
-chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
-    if (request.method == "getOpenureKeys")
-        sendResponse({keys: localStorage['openure_keys']});
-    else
-        sendResponse({}); // snub them.
-});
